dd7d186801d4531a0c3832553917a31caff7db36
monika <120088441+monikazanje@users.noreply.github.com>
2023-07-29 23:52:54 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,36 @@
+
+
+
+
+public class main {
+    public static void main(String[] args) {
+        int x=2;
+        int y=4;
+        System.out.println("product of these values =" + x*y);
+
+
+
+
+        char s='U';
+        int asciint=s;
+        System.out.println("asci value of U =" +asciint );
+
+
+        int length = 7;
+        int breadth=4;
+        int area=length*breadth;
+        System.out.println("area is" +area);
+
+        int num=4;
+        int cube=num*num*num;
+        System.out.println("the cube of 4 is ="+cube);
+
+        int a=2;
+        int b=3;
+         int swap=a;
+         a=b;
+         b=swap;
+        System.out.println("a is=" +a +"   b is ="+b);
+
+    }
+}
