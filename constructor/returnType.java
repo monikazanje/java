8c3733aaf223795e8137bd8d3feeb316afdeb4a2
monika <120088441+monikazanje@users.noreply.github.com>
2023-08-01 23:49:46 +0530

    Create returnType.java

---
+++ constructor/returnType.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class returnTypeCon {
+
+    void returnTypeCon(){
+        System.out.println("hello");
+    }
+
+    public static void main(String[] args) {
+        returnTypeCon s2=new returnTypeCon();
+        s2.returnTypeCon();
+
+    }
+
+
+}
