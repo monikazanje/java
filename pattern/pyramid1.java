f0be78734abc1c21b9257396c183449a076010da
monika <120088441+monikazanje@users.noreply.github.com>
2023-09-29 23:10:37 +0530

    Create pyramid1.java

---
+++ pattern/pyramid1.java
@@ -0,0 +1,24 @@
+   
+ //    * 
+ //   * * 
+ //  * * * 
+ // * * * * 
+
+public class butterfly {
+  public static void main(String [] args){
+    for(int i=5;i>0;i--){
+      for(int j=0;j<i;j++){
+        System.out.print(" ");
+      }
+      for(int k=4;k>=i;k--){
+        System.out.print("* ");
+      }
+      System.out.println();
+
+    }
+
+
+  }
+    
+}
+
