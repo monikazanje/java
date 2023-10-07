68c128f87b624804efc991c652ae8069e94e1176
monika <120088441+monikazanje@users.noreply.github.com>
2023-10-08 01:26:02 +0530

    Create string1.java

---
+++ string/string1.java
@@ -0,0 +1,20 @@
+// string :monika
+// string buffer:monika software
+
+
+public class string1 {
+    public static void main(String[] args) {
+                String s=new String("monika");
+                s.concat("software");
+                System.out.println("string :"+s);
+
+                StringBuffer sb=new StringBuffer("monika");
+                sb.append(" software");
+                System.out.println("string buffer:"+sb);
+
+
+        }
+
+
+
+}
